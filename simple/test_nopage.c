fcd5f960042560ed9deb60ba0e2c5626ac032fbf
Zhao Ertao <nathenzhao@pateo.com.cn>
2014-04-16 15:43:01 +0800

    simple using test

---
+++ simple/test_nopage.c
@@ -0,0 +1,28 @@
+//#include </work/apue/ourhdr.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+int main(int argc, char *argv[])
+{
+	int fdin, fdout;
+	void *src, *dst;
+	struct stat statbuf;
+	unsigned char sz[1024]={0};
+	if ((fdin = open("/dev/simplen", O_RDONLY)) < 0)
+		perror("can't open /dev/simple_remap for reading");
+
+	if ((src = mmap(0, 4096*2, PROT_READ, MAP_SHARED,
+					fdin, 0)) == MAP_FAILED)
+		perror("mmap error for simplen");
+
+	memcpy(sz, src, 11);
+	sz[10]='\0';
+	printf("%x\n", src);
+	printf("%s\n\n", sz);
+
+	memcpy(sz, src+4096, 11);
+	printf("%x\n", src+4096);
+	printf("%s\n", sz);
+
+	exit(0);
+}
